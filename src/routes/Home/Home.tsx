ee8b32cf8764a842375d30783888f4ba467734d6
Melina Mejia <melinamejia95@gmail.com>
2024-11-21 17:01:24 -0500

    feat: initial config to start website project

---
+++ src/routes/Home/Home.tsx
@@ -0,0 +1,14 @@
+import './Home.styles.scss';
+
+const Home = () => {
+  return (
+    <>
+      <h1>Hey there</h1>
+      <p className="read-the-docs">
+        I'm Melina
+      </p>
+    </>
+  )
+}
+
+export default Home;
